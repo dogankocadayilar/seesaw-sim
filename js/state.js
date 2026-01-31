8b0f3b884c00463e2c0be284b33d46759a529c15
Doğan Kocadayılar <dogankocadayilar@gmail.com>
2026-01-31 15:10:34 +0300

    add seesaw state model

---
+++ js/state.js
@@ -0,0 +1,23 @@
+// Object state weight, distanceFromCenter, side
+
+export const State = {
+  objects: [],
+  nextWeight: 0,
+  leftWeight: 0,
+  rightWeight: 0,
+  angle: 0,
+
+  reset() {
+    this.objects = [];
+    this.leftWeight = 0;
+    this.rightWeight = 0;
+    this.angle = 0;
+    this.generateNextWeight();
+  },
+  generateNextWeight() {
+    this.nextWeight = Math.floor(Math.random() * 10) + 1;
+  },
+  addObject(weight, distanceFromCenter, side) {
+    this.objects.push({ weight, distanceFromCenter, side });
+  },
+};
